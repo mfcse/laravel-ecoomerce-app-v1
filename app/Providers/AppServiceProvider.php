449c2ab34bf137c58519f3c0c1676f806248534f
Maruf <maruf.cu.cse09@gmail.com>
2021-07-10 12:34:18 +0600

    category lists added in menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use Facade\FlareClient\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $categories = Category::select(['name', 'slug'])->where('category_id', null)->get();
+        //adding data to all views
+        view()->share('categories', $categories);
     }
 }
