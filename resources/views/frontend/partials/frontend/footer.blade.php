c68d744fda981c351229b5a27073882d4aa44621
Maruf <maruf.cu.cse09@gmail.com>
2021-07-10 12:22:24 +0600

    home layout added

---
+++ resources/views/frontend/partials/frontend/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="text-muted">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">Back to top</a>
+      </p>
+      <p> &copy; {{config('app.name')}} All Rights Reserved {{date('Y')}}</p>
+      <p><a href="#">Privacy Policy</a>  <a href="#">Contact Us</a>.</p>
+    </div>
+  </footer>
